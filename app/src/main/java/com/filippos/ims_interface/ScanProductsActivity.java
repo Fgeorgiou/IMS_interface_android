210dee6fd7081c5eb12c3b5bd68d7416c0a4431b
Filippos Georgiou <fgeorgiou@amcstudent.edu.gr>
2018-04-16 19:52:56 +0300

    Scan Items Functionality v0.6
    - Created the FetchProduct AsyncTask to look for product information based on the scanning utility.
    
    - Displaying information if product is found else clearing text views and keeping barcode to enable addition to DB after function is created.
    
    - Minor UI changes in scan product activity.
    
    - Added comments in code.

--- app/src/main/java/com/filippos/ims_interface/ScanProductsActivity.java
+++ app/src/main/java/com/filippos/ims_interface/ScanProductsActivity.java
@@ -2,14 +2,178 @@ package com.filippos.ims_interface;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ScanProductsActivity extends AppCompatActivity {
+
+    public class FetchProductInfo extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setRequestMethod("GET");
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            TextView scannedProductNameTextView = findViewById(R.id.scannedProductNameTextView);
+            TextView scannedProductSupplierTextView = findViewById(R.id.scannedProductSupplierTextView);
+            TextView scannedProductCategoryTextView = findViewById(R.id.scannedProductCategoryTextView);
+            TextView scannedProductBarcodeValueTextView = findViewById(R.id.scannedProductBarcodeValueTextView);
+            TextView scannedProductPerPackValueTextView = findViewById(R.id.scannedProductPerPackValueTextView);
+            TextView scannedProductNetValueTextView = findViewById(R.id.scannedProductNetValueTextView);
+            TextView scannedProductGrossValueTextView = findViewById(R.id.scannedProductGrossValueTextView);
+            TextView scannedProductLeadValueTextView = findViewById(R.id.scannedProductLeadValueTextView);
+            TextView scannedProductStockValueTextView = findViewById(R.id.scannedProductStockValueTextView);
+
+            try {
+
+                //Getting the whole json response
+                JSONObject jsonResponseObject = new JSONObject(result).getJSONObject("response");
+
+                String request_code = jsonResponseObject.getString("request_code");
+
+                if(request_code.equals("200")){
+
+                    Toast.makeText(getApplicationContext(), "Found!", Toast.LENGTH_SHORT).show();
+
+                    //Extracting the necessary information from json response
+                    JSONObject jsonProductObject = new JSONObject(jsonResponseObject.toString()).getJSONObject("product");
+                    JSONObject jsonProductSupplier = new JSONObject(jsonProductObject.toString()).getJSONObject("supplier");
+                    JSONObject jsonProductStock = new JSONObject(jsonProductObject.toString()).getJSONObject("stock");
+                    JSONObject jsonProductCategory = new JSONObject(jsonProductObject.toString()).getJSONObject("category");
+
+                    //Setting the TextView values
+                    scannedProductNameTextView.setText(jsonProductObject.getString("name"));
+                    scannedProductSupplierTextView.setText(jsonProductSupplier.getString("name"));
+                    scannedProductCategoryTextView.setText(jsonProductCategory.getString("name"));
+                    scannedProductBarcodeValueTextView.setText(jsonProductObject.getString("barcode"));
+                    scannedProductPerPackValueTextView.setText(jsonProductObject.getString("unit_per_pack"));
+                    scannedProductNetValueTextView.setText(jsonProductObject.getString("unit_net_weight_gr"));
+                    scannedProductGrossValueTextView.setText(jsonProductObject.getString("unit_gross_weight_gr"));
+                    scannedProductLeadValueTextView.setText(jsonProductObject.getString("lead_days"));
+                    scannedProductStockValueTextView.setText(jsonProductStock.getString("quantity"));
+
+                } else {
+
+                    String product_barcode = jsonResponseObject.getString("barcode");
+
+                    scannedProductNameTextView.setText("Scan a Product..");
+                    scannedProductSupplierTextView.setText("");
+                    scannedProductCategoryTextView.setText("");
+                    scannedProductBarcodeValueTextView.setText(product_barcode);
+                    scannedProductPerPackValueTextView.setText("");
+                    scannedProductNetValueTextView.setText("");
+                    scannedProductGrossValueTextView.setText("");
+                    scannedProductLeadValueTextView.setText("");
+                    scannedProductStockValueTextView.setText("");
+
+                    Toast.makeText(getApplicationContext(), "Barcode not found..", Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (JSONException e) {
+
+//                orderProductRecognitionTextView.setText("Unknown Barcode");
+//                orderStockTextView.setText("");
+
+                Toast.makeText(getApplicationContext(), "Something went wrong! Please re-scan.", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_products);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
+
+                if(contents.matches("[0-9]{12,13}")) {
+
+                    if(contents.length() == 12) {
+
+                        FetchProductInfo fetchProductInfo = new FetchProductInfo();
+                        fetchProductInfo.execute(MainActivity.ngrokURL + "/api/products/show/0" + contents);
+
+                    } else if(contents.length() == 13){
+
+                        FetchProductInfo fetchProductInfo = new FetchProductInfo();
+                        fetchProductInfo.execute(MainActivity.ngrokURL + "/api/products/show/" + contents);
+
+                    }
+
+                }
+            } else
+            if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
 
     public void scannedProductNavigator(View view){
 
@@ -42,26 +206,6 @@ public class ScanProductsActivity extends AppCompatActivity {
             finish();
         }
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_products);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
-                Toast.makeText(getApplicationContext(), contents, Toast.LENGTH_LONG).show();
-            } else
-            if (resultCode == RESULT_CANCELED) {
-                // Handle cancel
-            }
-        }
     }
 
 }
